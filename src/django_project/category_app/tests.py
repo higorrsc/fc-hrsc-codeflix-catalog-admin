171037bb088f7a6ce5bc4233b2d4b73e302dd1ee
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-02-27 20:56:37 -0300

    feat: add pytest for Django and first test

--- src/django_project/category_app/tests.py
+++ src/django_project/category_app/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestCategoryAPI(APITestCase):
+    """
+    Test the Category API.
+    """
+
+    def test_list_categories(self):
+        """
+        Test that the API returns the expected list of categories.
+
+        When the API is called with GET /api/categories/, it should return a list
+        of categories with their id, name, description, and is_active status.
+
+        The expected result is a list of two categories with their respective
+        information.
+        """
+
+        url = "/api/categories/"
+        expected_data = [
+            {
+                "id": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+                "name": "Category 1",
+                "description": "Description 1",
+                "is_active": True,
+            },
+            {
+                "id": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e",
+                "name": "Category 2",
+                "description": "Description 2",
+                "is_active": True,
+            },
+        ]
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
