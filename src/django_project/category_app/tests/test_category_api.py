312ead024db0bac981f136a34cdebced7dbf6121
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-02-28 20:41:05 -0300

    feat: add api get category and tests

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,15 +1,67 @@
-from rest_framework.test import APITestCase
+import uuid
+
+import pytest
+from rest_framework.test import APIClient
 
 from django_project.category_app.repository import DjangoORMCategoryRepository
 from src.core.category.domain.category import Category
 
 
-class TestCategoryAPI(APITestCase):
+@pytest.fixture
+def category_movie() -> Category:
+    """
+    Fixture for a Category instance representing movies.
+
+    Returns:
+        Category: A Category object with name "Movie" and description "Movies category".
+    """
+
+    return Category(
+        name="Movie",
+        description="Movies category",
+    )
+
+
+@pytest.fixture
+def category_tv_show() -> Category:
+    """
+    Fixture for a Category instance representing TV shows.
+
+    Returns:
+        Category: A Category object with name "TV Show" and description "TV Show category".
+    """
+
+    return Category(
+        name="TV Show",
+        description="TV Show category",
+    )
+
+
+@pytest.fixture
+def category_repository() -> DjangoORMCategoryRepository:
+    """
+    Fixture for a DjangoORMCategoryRepository instance.
+
+    Returns:
+        DjangoORMCategoryRepository: An instance of the DjangoORMCategoryRepository
+        for interacting with the category data in the database.
+    """
+
+    return DjangoORMCategoryRepository()
+
+
+@pytest.mark.django_db
+class TestCategoryAPI:
     """
     Test the Category API.
     """
 
-    def test_list_categories(self):
+    def test_list_categories(
+        self,
+        category_movie: Category,
+        category_tv_show: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ):
         """
         Test that the API returns the expected list of categories.
 
@@ -20,18 +72,8 @@ class TestCategoryAPI(APITestCase):
         information.
         """
 
-        category_movie = Category(
-            name="Movie",
-            description="Movies category",
-        )
-        category_tv_show = Category(
-            name="TV Show",
-            description="TV Show category",
-        )
-
-        repository = DjangoORMCategoryRepository()
-        repository.save(category_movie)
-        repository.save(category_tv_show)
+        category_repository.save(category_movie)
+        category_repository.save(category_tv_show)
 
         url = "/api/categories/"
         expected_data = [
@@ -49,6 +91,73 @@ class TestCategoryAPI(APITestCase):
             },
         ]
 
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, expected_data)
+        response = APIClient().get(url)
+        assert response.status_code, 200
+        assert response.data, expected_data
+
+
+@pytest.mark.django_db
+class TestRetrieveAPI:
+    """
+    Test the Retrieve API.
+    """
+
+    def test_when_id_is_invalid_return_400(self):
+        """
+        Test that the API returns 400 when the given ID is invalid.
+
+        When the API is called with GET /api/categories/<id>/ and the given ID is invalid,
+        it should return a 400 error.
+
+        The expected result is a 400 status code.
+        """
+
+        url = "/api/categories/123456789/"
+        response = APIClient().get(url)
+
+        assert response.status_code, 400
+
+    def test_return_category_when_exists(
+        self,
+        category_movie: Category,
+        category_tv_show: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ):
+        """
+        Test that the API returns the expected category when the given ID exists.
+
+        When the API is called with GET /api/categories/<id>/ and the given ID exists,
+        it should return the category with its id, name, description, and is_active status.
+
+        The expected result is the category with its respective information.
+        """
+
+        category_repository.save(category_movie)
+        category_repository.save(category_tv_show)
+
+        url = f"/api/categories/{category_movie.id}/"
+        expected_data = {
+            "id": str(category_movie.id),
+            "name": category_movie.name,
+            "description": category_movie.description,
+            "is_active": category_movie.is_active,
+        }
+
+        response = APIClient().get(url)
+        assert response.status_code, 200
+        assert response.data, expected_data
+
+    def test_return_404_when_category_not_exists(self):
+        """
+        Test that the API returns 404 when the given ID does not exist.
+
+        When the API is called with GET /api/categories/<id>/ and the given ID does not exist,
+        it should return a 404 error.
+
+        The expected result is a 404 status code.
+        """
+
+        url = f"/api/categories/{uuid.uuid4()}/"
+
+        response = APIClient().get(url)
+        assert response.status_code, 404
--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,17 +1,29 @@
+import uuid
+
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
 from django_project.category_app.repository import DjangoORMCategoryRepository
+from src.core.category.application.exceptions import CategoryNotFound
+from src.core.category.application.use_cases.get_category import (
+    GetCategory,
+    GetCategoryRequest,
+)
 from src.core.category.application.use_cases.list_category import (
     ListCategory,
     ListCategoryRequest,
 )
+from src.core.category.domain.category import Category
 
 
 # Create your views here.
 class CategoryViewSet(viewsets.ViewSet):
+    """
+    ViewSet for handling category operations.
+    """
+
     def list(self, request: Request) -> Response:
         """
         Retrieve a list of categories.
@@ -40,5 +52,43 @@ class CategoryViewSet(viewsets.ViewSet):
 
         return Response(
             data=categories,
+            status=HTTP_200_OK,
+        )
+
+    def retrieve(self, request: Request, pk: None) -> Response:
+        """
+        Retrieve a category by its id.
+
+        Args:
+            request (Request): The request object containing request data.
+            pk (uuid.UUID): The id of the category to be retrieved.
+
+        Returns:
+            Response: A response object containing the category data.
+        """
+
+        try:
+            category_id = uuid.UUID(pk)
+            req = GetCategoryRequest(category_id)
+            use_case = GetCategory(DjangoORMCategoryRepository())
+            res = use_case.execute(req)
+        except ValueError:
+            return Response(
+                data={"detail": "Invalid category id"},
+                status=HTTP_400_BAD_REQUEST,
+            )
+        except CategoryNotFound:
+            return Response(
+                data={"detail": "Category not found"},
+                status=HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            data={
+                "id": str(res.id),
+                "name": res.name,
+                "description": res.description,
+                "is_active": res.is_active,
+            },
             status=HTTP_200_OK,
         )
