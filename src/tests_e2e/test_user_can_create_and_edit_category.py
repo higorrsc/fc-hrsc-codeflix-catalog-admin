eaf0be9f6d70e0546789c92d0f209ea9efe98a24
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-03-05 20:11:44 -0300

    feat: add e2e test

---
+++ src/tests_e2e/test_user_can_create_and_edit_category.py
@@ -0,0 +1,92 @@
+import pytest
+from rest_framework.test import APIClient
+
+
+@pytest.mark.django_db
+class TestCreateAndEditCategory:
+    def test_user_can_create_and_edit_category(self) -> None:
+        """
+        Verify that a user can create and edit a category, and that the edited
+        category data is correct.
+
+        This test creates a category, verifies that it was created, updates the
+        category, verifies that it was updated, deletes the category, and verifies
+        that it was deleted.
+        """
+        api_client = APIClient()
+
+        # Verify that list categories is empty
+        list_response = api_client.get("/api/categories/")
+        assert list_response.status_code == 200
+
+        # Create category
+        create_response = api_client.post(
+            "/api/categories/",
+            data={"name": "Test Category", "description": "Test Category Description"},
+            format="json",
+        )
+        assert create_response.status_code == 201
+        category_response_id = create_response.data["id"]
+
+        # Verify that category was created
+        list_response = api_client.get("/api/categories/")
+        assert list_response.status_code == 200
+        assert len(list_response.data["data"]) == 1
+        assert list_response.data == {
+            "data": [
+                {
+                    "id": category_response_id,
+                    "name": "Test Category",
+                    "description": "Test Category Description",
+                    "is_active": True,
+                }
+            ]
+        }
+
+        # Verify that category is active
+        get_response = api_client.get(f"/api/categories/{category_response_id}/")
+        assert get_response.status_code == 200
+        assert get_response.data == {
+            "data": {
+                "id": category_response_id,
+                "name": "Test Category",
+                "description": "Test Category Description",
+                "is_active": True,
+            }
+        }
+
+        # Update category
+        update_response = api_client.put(
+            f"/api/categories/{category_response_id}/",
+            data={
+                "name": "Test Category Updated",
+                "description": "Test Category Description Updated",
+                "is_active": False,
+            },
+            format="json",
+        )
+        assert update_response.status_code == 204
+
+        # Verify that category was updated
+        list_response = api_client.get("/api/categories/")
+        assert list_response.status_code == 200
+        assert len(list_response.data["data"]) == 1
+        assert list_response.data == {
+            "data": [
+                {
+                    "id": category_response_id,
+                    "name": "Test Category Updated",
+                    "description": "Test Category Description Updated",
+                    "is_active": False,
+                }
+            ]
+        }
+
+        # Delete category
+        delete_response = api_client.delete(f"/api/categories/{category_response_id}/")
+        assert delete_response.status_code == 204
+
+        # Verify that category was deleted
+        list_response = api_client.get("/api/categories/")
+        assert list_response.status_code == 200
+        assert len(list_response.data["data"]) == 0
