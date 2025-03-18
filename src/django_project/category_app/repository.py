1e9e045e0895bab0b839a5855cd85d9f7698c5e2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-03-18 20:41:16 -0300

    feat: add mapper to category repository

--- src/django_project/category_app/repository.py
+++ src/django_project/category_app/repository.py
@@ -12,34 +12,65 @@ class DjangoORMCategoryRepository(CategoryRepository):
     """
 
     def __init__(self, category_model: CategoryModel | None = None):
+        """
+        Initialize the DjangoORMCategoryRepository with an optional CategoryModel.
+
+        Args:
+            category_model (CategoryModel | None): An optional CategoryModel instance.
+                If not provided, the default CategoryModel is used.
+        """
+
         self.category_model = category_model or CategoryModel
 
     def save(self, category: Category):
-        category_data = {
-            "id": category.id,
-            "name": category.name,
-            "description": category.description,
-            "is_active": category.is_active,
-        }
+        """
+        Save a category to the Django ORM database.
 
-        self.category_model.objects.create(**category_data)
+        Args:
+            category (Category): The category to be saved.
+        """
+
+        category_model = CategoryModelMapper.to_model(category)
+        category_model.save()
 
     def get_by_id(self, category_id: uuid.UUID) -> Category | None:
+        """
+        Retrieve a category by its ID from the Django ORM database.
+
+        Args:
+            category_id (uuid.UUID): The ID of the category to be retrieved.
+
+        Returns:
+            Category | None: The category with the given ID, or None if it doesn't exist.
+        """
+
         try:
             category = self.category_model.objects.get(pk=category_id)
-            return Category(
-                id=category.id,
-                name=category.name,
-                description=category.description,  # type: ignore
-                is_active=category.is_active,
-            )
+            return CategoryModelMapper.to_entity(category)
         except self.category_model.DoesNotExist:
             return None
 
     def delete(self, category_id: uuid.UUID):
+        """
+        Delete a category by its ID from the Django ORM database.
+
+        Args:
+            category_id (uuid.UUID): The ID of the category to be deleted.
+        """
+
         self.category_model.objects.filter(pk=category_id).delete()
 
     def update(self, category: Category):
+        """
+        Update a category in the Django ORM database.
+
+        Args:
+            category (Category): The category to be updated.
+
+        Raises:
+            ValueError: If the category ID does not exist in the database.
+        """
+
         category_data = {
             "name": category.name,
             "description": category.description,
@@ -49,12 +80,58 @@ class DjangoORMCategoryRepository(CategoryRepository):
         self.category_model.objects.filter(pk=category.id).update(**category_data)
 
     def list(self) -> List[Category]:
+        """
+        List all categories from the Django ORM database.
+
+        Returns:
+            list[Category]: A list of all categories in the database.
+        """
+
         return [
-            Category(
-                id=category.id,
-                name=category.name,
-                description=category.description,  # type: ignore
-                is_active=category.is_active,
-            )
-            for category in self.category_model.objects.all()
+            CategoryModelMapper.to_entity(category_model)
+            for category_model in self.category_model.objects.all()
         ]
+
+
+class CategoryModelMapper:
+    """
+    A class for mapping between Category and CategoryModel.
+    """
+
+    @staticmethod
+    def to_model(category: Category) -> CategoryModel:
+        """
+        Maps a Category entity to a CategoryModel.
+
+        Args:
+            category (Category): The category entity to be mapped.
+
+        Returns:
+            CategoryModel: The mapped CategoryModel.
+        """
+
+        return CategoryModel(
+            id=category.id,
+            name=category.name,
+            description=category.description,
+            is_active=category.is_active,
+        )
+
+    @staticmethod
+    def to_entity(category_model: CategoryModel) -> Category:
+        """
+        Maps a CategoryModel to a Category entity.
+
+        Args:
+            category_model (CategoryModel): The category model to be mapped.
+
+        Returns:
+            Category: The mapped Category entity.
+        """
+
+        return Category(
+            id=category_model.id,
+            name=category_model.name,
+            description=category_model.description,  # type: ignore
+            is_active=category_model.is_active,
+        )
