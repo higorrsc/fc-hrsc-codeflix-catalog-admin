aea6da62c8fbe0b5bc46ffc32e69b34351da0ca3
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-03-17 21:14:08 -0300

    feat: add CastMemberNotFound exception

--- src/core/cast_member/application/exceptions.py
+++ src/core/cast_member/application/exceptions.py
@@ -2,3 +2,9 @@ class InvalidCastMember(Exception):
     """
     Exception raised when the data provided to create a CastMember is invalid
     """
+
+
+class CastMemberNotFound(Exception):
+    """
+    Exception raised when a CastMember is not found
+    """
