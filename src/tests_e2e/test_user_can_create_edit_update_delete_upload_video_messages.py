4450f904414db61d12cad1406aa43f8806a2a1fd
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-01 20:56:33 -0300

    feat: add first version of test e2e

---
+++ src/tests_e2e/test_user_can_create_edit_update_delete_upload_video_messages.py
@@ -0,0 +1,187 @@
+import time
+import uuid
+
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
+from rest_framework.test import APIClient
+
+from src.core.cast_member.domain.cast_member import CastMember, CastMemberType
+from src.core.category.domain.category import Category
+from src.core.genre.domain.genre import Genre
+from src.core.video.domain.value_objects import MediaStatus, MediaType
+from src.core.video.infra.video_converted_producer import VideoConvertedRabbitMQProducer
+from src.django_project.cast_member_app.repository import DjangoORMCastMemberRepository
+from src.django_project.category_app.repository import DjangoORMCategoryRepository
+from src.django_project.genre_app.repository import DjangoORMGenreRepository
+
+
+@pytest.mark.django_db
+class TestCreateEditUpdateDeleteAndUploadVideo:
+    """
+    Test class for testing user can create, edit and delete a video with file upload
+    """
+
+    def test_user_can_create_edit_update_delete_and_upload_video(self):
+        """
+        Verify that a user can create, edit, update, delete, and upload a video,
+        and that the edited video data is correct.
+
+        This test creates a video, verifies that it was created, updates the video,
+        verifies that it was updated, deletes the video, and verifies that it was
+        deleted.
+        """
+        api_client = APIClient()
+
+        movie_category_id = uuid.uuid4()
+        genre_action_id = uuid.uuid4()
+        genre_adventure_id = uuid.uuid4()
+        cast_member_actor_id = uuid.uuid4()
+        cast_member_director_id = uuid.uuid4()
+
+        category_repository = DjangoORMCategoryRepository()
+        genre_repository = DjangoORMGenreRepository()
+        cast_member_repository = DjangoORMCastMemberRepository()
+
+        category_repository.save(
+            Category(
+                id=movie_category_id,
+                name="Movie",
+            )
+        )
+
+        genre_repository.save(
+            Genre(
+                id=genre_action_id,
+                name="Action",
+                categories={movie_category_id},
+            )
+        )
+        genre_repository.save(
+            Genre(
+                id=genre_adventure_id,
+                name="Adventure",
+                categories={movie_category_id},
+            )
+        )
+
+        cast_member_repository.save(
+            CastMember(
+                id=cast_member_actor_id,
+                name="Sam Worthington",
+                type=CastMemberType.ACTOR,
+            )
+        )
+        cast_member_repository.save(
+            CastMember(
+                id=cast_member_director_id,
+                name="James Cameron",
+                type=CastMemberType.DIRECTOR,
+            )
+        )
+
+        list_response = api_client.get("/api/videos/")
+        assert list_response.status_code == HTTP_200_OK  # type: ignore
+
+        data = {
+            "title": "Avatar",
+            "description": "A marine on an alien planet",
+            "duration": 162,
+            "launch_year": 2009,
+            "rating": "AGE_14",
+            "categories": [
+                str(movie_category_id),
+            ],
+            "genres": [
+                str(genre_action_id),
+                str(genre_adventure_id),
+            ],
+            "cast_members": [
+                str(cast_member_actor_id),
+                str(cast_member_director_id),
+            ],
+        }
+
+        create_response = api_client.post(
+            path="/api/videos/",
+            data=data,
+            format="json",
+        )
+        assert create_response.status_code == HTTP_201_CREATED  # type: ignore
+        assert create_response.data["id"]  # type: ignore
+
+        video_id = create_response.data["id"]  # type: ignore
+
+        list_response = api_client.get("/api/videos/")
+        assert list_response.status_code == HTTP_200_OK  # type: ignore
+        assert len(list_response.data["data"]) == 1  # type: ignore
+
+        updated_data = {
+            "title": "Avatar Updated",
+            "description": "A marine on an alien planet",
+            "duration": 162,
+            "launch_year": 2009,
+            "published": False,
+            "rating": "AGE_14",
+            "categories": [
+                str(movie_category_id),
+            ],
+            "genres": [
+                str(genre_adventure_id),
+            ],
+            "cast_members": [
+                str(cast_member_director_id),
+            ],
+        }
+
+        update_response = api_client.put(
+            path=f"/api/videos/{video_id}/",
+            data=updated_data,
+            format="json",
+        )
+
+        assert update_response.status_code == HTTP_204_NO_CONTENT  # type: ignore
+
+        list_response = api_client.get("/api/videos/")
+        assert list_response.status_code == HTTP_200_OK  # type: ignore
+        assert len(list_response.data["data"]) == 1  # type: ignore
+        assert list_response.data["data"], updated_data  # type: ignore
+
+        upload_file = SimpleUploadedFile(
+            "video.mp4",
+            b"Fake video content",
+            "video/mp4",
+        )
+        upload_data = {
+            "video_file": upload_file,
+        }
+        patch_response = api_client.patch(
+            path=f"/api/videos/{video_id}/",
+            data=upload_data,
+            format="multipart",
+        )
+        assert patch_response.status_code == HTTP_200_OK  # type: ignore
+
+        producer = VideoConvertedRabbitMQProducer()
+        message = {
+            "error": "",
+            "video": {
+                "resource_id": f"{video_id}.{MediaType.VIDEO}",
+                "encoded_video_folder": "/path/to/encoded/video",
+            },
+            "status": MediaStatus.COMPLETED,
+        }
+        producer.publish(message=message)
+        producer.close()
+        time.sleep(10)
+
+        get_response = api_client.get(f"/api/videos/{video_id}/")
+        assert get_response.status_code == HTTP_200_OK  # type: ignore
+        assert get_response.data["id"] == video_id  # type: ignore
+
+        delete_response = api_client.delete(path=f"/api/videos/{video_id}/")
+        assert delete_response.status_code == HTTP_204_NO_CONTENT  # type: ignore
+
+        list_response = api_client.get("/api/videos/")
+        assert list_response.status_code == HTTP_200_OK  # type: ignore
+        assert len(list_response.data["data"]) == 0  # type: ignore
